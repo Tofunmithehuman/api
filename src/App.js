068a827dba26b24d4141a5f963203b8b86b3fcc7
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2023-10-30 12:26:41 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import UserList from './UserList';
+import { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { setUsersList } from "./Redux/UsersSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { list } = useSelector(state => state.users)
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(res => dispatch(setUsersList(res.data)))
+      .catch(err => console.log(err))
+  }, [dispatch]);
+
+
+  console.log('data from state:', list);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserList users={list} />
     </div>
   );
 }
